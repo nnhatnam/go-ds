24c6b247aa8abe25a908cf738c34e1e3881d8f57
Nam Nguyen <nnhatnam91@gmail.com>
2020-12-19 22:20:05 -0600

    Finalize API
    After a lot of consideration, decided to go with std API

--- linearlists/sllist/list.go
+++ linearlists/sllist/list.go
@@ -1,7 +1,5 @@
 package sllist
 
-
-
 // Element is a node of a linked list.
 type Element struct {
 
@@ -10,29 +8,44 @@ type Element struct {
 	// The list to which this Element belongs.
 	list *List
 
-	//The value stored with this node
-	value interface{}
+	//The value stored with this element
+	Value interface{}
 }
 
+//Next returns the next list element or nil
 func (n *Element) Next() *Element {
 	return n.next
 }
 
+//List represents a singly linked list.
 type List struct {
-	head *Element
-	tail *Element
-	//root sentinel
-	length int //  current list length excluding (this) sentinel Element
+
+	//this linked list use dummy element technical
+
+	head *Element //head points to the dummy element
+	tail *Element //tail points to the last list element or dummy element if the list is empty
+
+	len int //  current list len excluding (this) dummy Element
 }
 
-// Init initializes or clears list l.
-func (l *List) Init() *List {
-	//this linked list use dummy node technical
-	node := &Element{list: l, value: nil}
-	l.head = node
-	l.tail = node
-	l.length = 0
-	return l
+//Len returns the number of Elements of list l. The complexity is O(1).
+func (l *List) Len() int { return l.len }
+
+//Front returns the first element of list l or nil if l is empty. The time complexity is O(1)
+func (l *List) Front() *Element {
+	if l.len == 0 {
+		return nil
+	}
+	return l.head.next
+}
+
+//Back returns the last element of list l or nil if l is empty. The time complexity is O(1)
+func (l *List) Back() *Element {
+	if l.len == 0 {
+		return nil
+	}
+
+	return l.tail
 }
 
 // lazyInit lazily initializes a zero List value.
@@ -42,40 +55,123 @@ func (l *List) lazyInit() {
 	}
 }
 
+// Init initializes or clears list l.
+func (l *List) Init() *List {
+
+	node := &Element{list: l, Value: nil}
+	l.head, l.tail, l.len = node , node, 0
+	return l
+}
+
+// New returns an initialized list.
 func New() *List{
 	return new(List).Init()
 }
 
-//return the real head of list l
-func (l *List) listHead() *Element {
-	return l.head.next
-}
 
-//return the tail of list l
-func (l *List) listTail() *Element {
-	return l.tail
-}
+//insert an Element e after the Element at, increments l.len, and return n, update tail if needed.
+//Element at must not be nil. Element at must not belong to list l, so any call to l.insert must check if at NOT belong
+//to list l
+func (l *List) insert(e, at *Element) *Element {
+	e.next = at.next
+	at.next = e
+	l.len++
 
-func (l *List) append(value interface{})  {
-	if l.head == nil {
-		l.lazyInit()
+	if l.tail == at {
+		l.tail = e
 	}
-	l.tail.next = &Element{list: l, value:value}
-	l.tail = l.tail.next
-	l.length += 1
+	return e
+}
 
+// insertValue is a convenience wrapper for insert(&Element{Value: v}, at).
+func (l *List) insertValue(v interface{}, at *Element) *Element {
+	return l.insert(&Element{Value:v, list: l}, at)
+}
+
+// remove removes the Element e from list l (if e belong to the list), decrements l.len, and returns e. The time complexity is O(n)
+func (l *List) remove(e *Element) *Element {
+
+	//iterate over the list l, starting from dummy element, to find the element el before element e then remove e and update
+	//el reference
+	for el := l.head; el != nil; el = el.next {
+		if el.next == e {
+			//el is previous Element of e
+			el.next = e.next
+
+			if l.tail == e {
+				l.tail = el
+			}
+			e.next = nil
+			e.list = nil
+			l.len--
+			return e
+		}
+	}
+
+	return nil
+}
+
+type lookupFunc func(e *Element) bool
+
+//lookup loops through list l from dummy element and call function f for each visited element.
+//If f() return true then stop the loop and return the element
+func (l *List) lookup(f lookupFunc) *Element {
+
+	//loop from dummy node
+	for e := l.head; e != nil; e = e.next {
+		if f(e) {
+			return e
+		}
+	}
+	return nil
+}
+
+
+//move moves e next to at and return e. e and at must belong to the list l
+func (l *List) move(e, at *Element) *Element {
+
+	if e == at || at.next == e {
+		return e
+	}
+
+	if l.head.next == e {
+		l.head.next = e.next
+
+	} else {
+		nodeBeforeE := l.lookup(func(node *Element) bool {
+			return node.next == e
+		})
+		nodeBeforeE.next = e.next
+
+	}
+
+	afterAt := at.next
+	e.next = afterAt
+	at.next = e
+
+	if l.tail == at {
+		l.tail = e
+	}
+	return e
+}
+
+//append appends value into list l. List l must be initialized
+func (l *List) append(value interface{}) *Element {
+
+	e := &Element{list: l, Value:value}
+	l.tail.next = e
+	l.tail = l.tail.next
+	l.len += 1
+	return e
 }
 
 func (l *List) appendMany(values ...interface{}) *Element {
-	if l.head == nil {
-		l.lazyInit()
-	}
 
 	var elem *Element
 	for _, v := range values {
-		l.tail.next = &Element{list: l, value:v}
+		l.tail.next = &Element{list: l, Value:v}
 		l.tail = l.tail.next
-		l.length += 1
+		l.len += 1
 		if elem == nil {
 			elem = l.tail
 		}
@@ -83,93 +179,83 @@ func (l *List) appendMany(values ...interface{}) *Element {
 	return elem
 }
 
-
-
+//prepend insert value into the front of list l, after dummy element. List l must be initialized
 func (l *List) prepend(value interface{}) *Element {
 
-	if l.head == nil {
-		l.lazyInit()
-	}
-	node := &Element{
+	e := &Element{
 		list: l,
 		next:l.head.next,
-		value:value,
+		Value:value,
 	}
-	l.head.next = node
-	l.length++
-	return node
+
+	l.head.next = e
+	if l.len == 0 {
+		l.tail = e
+	}
+	l.len++
+	return e
 }
 
 func (l *List) prependMany(values ...interface{}) *Element {
-	if l.head == nil {
-		l.lazyInit()
-	}
 
 	for _ , v := range values {
 		l.head.next = &Element{
 			list: l,
 			next:l.head.next,
-			value:v,
+			Value:v,
 		}
-		l.length++
+		l.len++
 	}
 	return l.head.next
 }
 
-//Len returns the number of Elements of list l. The complexity is O(1).
-func (l *List) Len() int { return l.length }
 
 //First returns the first value of list l or nil if l is empty. The complexity is O(1)
 func (l *List) First() interface{} {
-	if l.length > 0 {
-		return l.head.next.value
+	if l.len > 0 {
+		return l.head.next.Value
 	}
 	return nil
 }
 
-//FirstElement returns the first element of list l or nil if l is empty. The complexity is O(1)
-func (l *List) FirstElement() *Element {
-	return l.head.next
-}
 
 //Last returns the last value of list l or nil if l is empty. The complexity is O(1)
 func (l *List) Last() interface{} {
-	if l.length > 0 {
-		return l.tail.value
+	if l.len > 0 {
+		return l.tail.Value
 	}
 	return nil
 }
 
-func (l *List) LastElement() *Element {
-	if l.length > 0 {
-		return l.tail
-	}
-	return nil
-}
+
 
 func (l *List) Append(values ...interface{}) *Element {
+	l.lazyInit()
 	return l.appendMany(values)
 }
 
 func (l *List) Prepend(values ...interface{}) *Element {
+	l.lazyInit()
 	return l.prependMany(values)
 }
 
 //PushFront inserts a new Element with value v at the beginning of list l. The complexity is O(1)
-func (l *List) PushFront(v interface{}) {
-	l.prepend(v)
+func (l *List) PushFront(v interface{}) *Element {
+	l.lazyInit()
+	return l.prepend(v)
 }
 
 //Push inserts a new Element with value v at the end of list l. The complexity is O(1)
-func (l *List) PushBack(v interface{})  {
-	l.append(v)
+func (l *List) PushBack(v interface{}) *Element {
+	l.lazyInit()
+	return l.append(v)
 }
 
 //Pop removes the last Element from list l and returns that Element. The complexity is O(n)
 func (l *List) PopBack() interface{} {
-	if l.length > 0 {
+	if l.len > 0 {
 		if e := l.remove(l.tail); e != nil {
-			return e.value
+			return e.Value
 		}
 	}
 
@@ -178,29 +264,9 @@ func (l *List) PopBack() interface{} {
 
 //Pop removes the first Element from list l and returns that Element. The complexity is O(n)
 func (l *List) PopFront() interface{} {
-	if l.length > 0 {
+	if l.len > 0 {
 		if e := l.remove(l.head.next); e != nil {
-			return e.value
-		}
-	}
-
-	return nil
-}
-
-// remove removes the Element e from list l (if e belong to the list), decrements l.len, and returns e. The complexity is O(n)
-func (l *List) remove(e *Element) *Element {
-
-	for el := l.head; el != nil; el = el.next {
-		if el.next == e {
-			//el is previous Element of e
-			el.next = e.next
-			if l.tail == e {
-				l.tail = el
-			}
-			e.next = nil
-			e.list = nil
-			l.length--
-			return e
+			return e.Value
 		}
 	}
 
@@ -222,45 +288,6 @@ func (l *List) Traverse(f IterateFunc){
 
 }
 
-
-type lookupFunc func(e *Element) bool //private find by node (index doesn't need, because we can access it in private level)
-
-func (l *List) lookup(f lookupFunc) *Element {
-
-	//loop from dummy node
-	for e := l.head; e != nil; e = e.next {
-		if f(e) {
-			return e
-		}
-	}
-	return nil
-}
-
-
-
-
-
-
-//insert an Element e after the Element at, increments l.len, and return n, update tail if needed. Element at must not be nil
-func (l *List) insert(e, at *Element) *Element {
-	if e.list != at.list {
-		return nil
-	}
-	if l.tail == at {
-		l.tail = e
-	}
-	e.next = at.next
-	at.next = e
-
-	l.length++
-	return e
-}
-
-// insertValue is a convenience wrapper for insert(&Element{Value: v}, at).
-func (l *List) insertValue(v interface{}, at *Element) *Element {
-	return l.insert(&Element{value:v,}, at)
-}
-
 // InsertAfter inserts a new Element e with value v immediately after an Element that mark iterator pointing in and returns the value.
 // If mark is not an Element of l, the list is not modified.
 // The mark must not be nil.
@@ -274,7 +301,9 @@ func (l *List) InsertAfter(v interface{}, mark *Element) *Element {
 }
 
 
-//Note later
+// InsertBefore inserts a new element e with value v immediately before mark and returns e.
+// If mark is not an element of l, the list is not modified.
+// The mark must not be nil.
 func (l *List) InsertBefore(v interface{}, mark *Element) *Element {
 
 	if mark.list != l {
@@ -285,39 +314,14 @@ func (l *List) InsertBefore(v interface{}, mark *Element) *Element {
 	elem := l.lookup(func(n *Element) bool {
 		return n.next == mark
 	})
-
 	return l.insertValue(v, elem)
 }
 
-//move node n after node at
-func (l *List) move(n, at *Element) *Element {
 
-	if n == at || at.next == n {
-		return n
-	}
 
-	if l.head.next == n {
-		l.head.next = n.next
-
-	} else {
-		nodeBeforeN := l.lookup(func(node *Element) bool {
-			return node.next == n
-		})
-		nodeBeforeN.next = n.next
-
-	}
-
-	afterAt := at.next
-	n.next = afterAt
-	at.next = n
-
-	if l.tail == at {
-		l.tail = n
-	}
-	return n
-}
-
-//need to re-implement
+// MoveAfter moves element e to its new position after mark.
+// If e or mark is not an element of l, or e == mark, the list is not modified.
+// The element and mark must not be nil.
 func (l *List) MoveAfter(e, mark *Element) {
 	if e.list != l || e == mark || mark.list != l {
 		return
@@ -359,14 +363,12 @@ func (l *List) MoveToFront(e *Element){
 	prev := l.lookup(func(node *Element) bool {
 		return node.next == e
 	})
-
 	prev.next = e.next
 	e.next = l.head.next
-
 	if e == l.tail {
 		l.tail = prev
 	}
-	l.head = e
+	l.head.next = e
 }
 
 
@@ -377,41 +379,47 @@ func (l *List) PushBackList(other *List){
 	//can't use traverse due to infinite loop if the list push back itself
 	for i, elem := other.Len(), other.head.next; i > 0; i, elem = i - 1,  elem.next {
 		if l.head.next == nil {
-			l.append(elem.value)
+			l.append(elem.Value)
 		} else {
-			l.insertValue(elem.value, l.tail )
+			l.insertValue(elem.Value, l.tail )
 		}
 		//nodes = append(nodes, node)
 	}
-	//fmt.Println("out ", l.length)
+	//fmt.Println("out ", l.len)
 }
 
 
-//need implement
+// PushFrontList inserts a copy of another list at the front of list l.
+// The lists l and other may be the same. They must not be nil.
 func (l *List) PushFrontList(other *List){
 	l.lazyInit()
-	tail := l.tail
+	//tail := l.tail
+	pointer := l.head
 	for i, elem := other.Len(), other.head.next; i > 0; i, elem = i - 1,  elem.next {
-		l.append(elem.value)
+
+		l.insertValue(elem.Value, pointer)
+		pointer = pointer.next
 		//nodes = append(nodes, node)
 	}
 
-	if tail != nil && l.tail != tail {
-		l.tail.next = l.head.next
-		l.head.next = tail.next
-		tail.next = nil
-	}
+	//if tail != nil && l.tail != tail {
+	//	l.tail.next = l.head.next
+	//	l.head.next = tail.next
+	//	tail.next = nil
+	//}
 }
 
 
 
-//need implement
+// Remove removes e from l if e is an element of list l.
+// It returns the element value e.Value.
+// The element must not be nil.
 func (l *List) Remove(e *Element) interface{}{
 	if e.list == l {
 		l.remove(e)
 	}
 
-	return e.value
+	return e.Value
 }
 
 //Removes all the Elements from this list.
--- linearlists/sllist/list_test.go
+++ linearlists/sllist/list_test.go
@@ -14,11 +14,8 @@ type TestStruct struct {
 }
 
 func printValues(l *List) {
-	values := make([]interface{}, l.length)
+	values := make([]interface{}, l.len)
 
-	for it := l.Iterator(); !it.End(); it.Next()
-
-	}
 	l.Traverse(func(n *Element, i int){
 		values[i] =  n.Value
 	})
@@ -35,7 +32,7 @@ func manualCount(l *List) int {
 }
 
 func getNodeArr(l *List) []*Element {
-	nodes := make([]*Element, l.length)
+	nodes := make([]*Element, l.len)
 	l.Traverse(func(n *Element, i int){
 		nodes[i] = n
 	})
@@ -51,24 +48,26 @@ func checkListLen(t *testing.T, l *List, len int) bool {
 }
 
 func checkListPointers(t *testing.T, l *List, nodes []*Element){
-	//root := l.root
 
 	if !checkListLen(t, l, len(nodes)) {
 		return
 	}
 
 	//zero length
+
 	if len(nodes) == 0 {
-		if l.head != nil || l.tail != l.head {
+
+		if l.tail != l.head {
 			t.Errorf("l.root.head, l.tail both should be nil")
 		}
 		return
 	}
 
 	//lenght > 0
+
 	l.Traverse(func(node *Element, i int) {
 		if i == 0 {
-			if node != l.head {
+			if node != l.head.next {
 				t.Errorf("node at position 0 should be the head of the list: %p <-> %p ", node, l.head)
 				return
 			}
@@ -80,6 +79,7 @@ func checkListPointers(t *testing.T, l *List, nodes []*Element){
 			}
 		}
 		if node != nodes[i] {
+			printValues(l)
 			t.Errorf("node are not repectively equal: %p <-> %p ", node, nodes[i])
 		}
 	})
@@ -89,37 +89,40 @@ func checkListPointers(t *testing.T, l *List, nodes []*Element){
 func TestNewList(t *testing.T) {
 
 	list := &List{}
-	if list.First() != nil {
+	if list.Front() != nil {
 		t.Error("(&List{}).Front() should be nil")
 	}
 	if list.Last() != nil {
 		t.Error("(&List{}).Back() should be nil")
 	}
+
 	checkListPointers(t, list, []*Element{})
 
 	//test New list without element
 
 	list = New()
-	if list.First() != nil {
+	if list.Front() != nil {
 		t.Error("(New().Front() should be nil")
 	}
 	if list.Last() != nil {
 		t.Error("(New().Back() should be nil")
 	}
+
 	checkListPointers(t, list, []*Element{})
 
 
 	//test New list with single element
-	list = New(1)
-	if list.First() != list.head {
-		t.Error("New(1).Front() should equal to list.root.head")
+	list = New()
+	list.PushFront(1)
+	if list.Front() != list.head.next {
+		t.Error("Front() should equal to list.head.next")
 	}
-	if list.Last() != list.tail {
-		t.Error("(New(1).Back() should equal to list.root.tail")
+	if list.Back() != list.tail {
+		t.Error("Back() should equal to list.tail")
 	}
 
-	if list.length != 1 || manualCount(list) != 1 {
-		t.Errorf("expected 1, got %v", list.length)
+	if list.len != 1 || manualCount(list) != 1 {
+		t.Errorf("expected 1, got %v", list.len)
 		return
 	}
 
@@ -127,16 +130,17 @@ func TestNewList(t *testing.T) {
 	checkListPointers(t, list, getNodeArr(list))
 
 	//test New list with multiple elements
-	list = New(1, "a", true, 1.1, TestStruct{"test struct"})
-	if list.First() != list.head.next {
-		t.Error("List with multiple elements First() should equal to list.root.head")
+	list = New()
+	list.appendMany(1, "a", true, 1.1, TestStruct{"test struct"})
+	if list.Front() != list.head.next {
+		t.Error("List with multiple elements First() should equal to list.head")
 	}
-	if list.Last() != list.tail {
-		t.Error("List with multiple elements Last() should equal to list.root.tail")
+	if list.Back() != list.tail {
+		t.Error("List with multiple elements Last() should equal to list.tail")
 	}
 
-	if list.length != 5 || manualCount(list) != 5 {
-		t.Errorf("expected 5, got %v", list.length)
+	if list.len != 5 || manualCount(list) != 5 {
+		t.Errorf("expected 5, got %v", list.len)
 		return
 	}
 	checkListPointers(t, list, getNodeArr(list))
@@ -160,8 +164,8 @@ func TestList(t *testing.T) {
 	// Bigger list
 	e2 := l.PushFront(2)
 	e1 := l.PushFront(1)
-	e3 := l.Push(3)
-	e4 := l.Push("banana")
+	e3 := l.PushBack(3)
+	e4 := l.PushBack("banana")
 	checkListPointers(t, l, []*Element{e1, e2, e3, e4})
 
 	l.Remove(e2)
@@ -183,11 +187,14 @@ func TestList(t *testing.T) {
 	l.MoveToBack(e3) // should be no-op
 	checkListPointers(t, l, []*Element{e1, e4, e3})
 
+
 	e2 = l.InsertBefore(2, e1) // insert before front
 	checkListPointers(t, l, []*Element{e2, e1, e4, e3})
 	l.Remove(e2)
+
 	e2 = l.InsertBefore(2, e4) // insert before middle
 	checkListPointers(t, l, []*Element{e1, e2, e4, e3})
+
 	l.Remove(e2)
 	e2 = l.InsertBefore(2, e3) // insert before back
 	checkListPointers(t, l, []*Element{e1, e4, e2, e3})
@@ -205,7 +212,7 @@ func TestList(t *testing.T) {
 
 	// Check standard iteration.
 	sum := 0
-	for e := l.First(); e != nil; e = e.Next() {
+	for e := l.Front(); e != nil; e = e.Next() {
 		if i, ok := e.Value.(int); ok {
 			sum += i
 		}
@@ -216,7 +223,7 @@ func TestList(t *testing.T) {
 
 	// Clear all elements by iterating
 	var next *Element
-	for e := l.First(); e != nil; e = next {
+	for e := l.Front(); e != nil; e = next {
 		next = e.Next()
 		l.Remove(e)
 	}
@@ -230,7 +237,7 @@ func checkList(t *testing.T, l *List, es []interface{}) {
 
 	i := 0
 
-	for e := l.First(); e != nil; e = e.Next() {
+	for e := l.Front(); e != nil; e = e.Next() {
 		le := e.Value.(int)
 		if le != es[i] {
 			t.Errorf("elt[%d].Value = %v, want %v", i, le, es[i])
@@ -243,20 +250,17 @@ func TestExtending(t *testing.T) {
 	l1 := New()
 	l2 := New()
 
-	l1.Push(1)
-	l1.Push(2)
-	l1.Push(3)
+	l1.PushBack(1)
+	l1.PushBack(2)
+	l1.PushBack(3)
 
-	l2.Push(4)
-	l2.Push(5)
+	l2.PushBack(4)
+	l2.PushBack(5)
 
 	l3 := New()
-	l3.PushFrontList(l1)
-
+	l3.PushBackList(l1)
 	checkList(t, l3, []interface{}{1, 2, 3})
-
-	l3.PushFrontList(l2)
-
+	l3.PushBackList(l2)
 	checkList(t, l3, []interface{}{1, 2, 3, 4, 5})
 
 	l3 = New()
@@ -295,11 +299,11 @@ func TestExtending(t *testing.T) {
 
 func TestRemove(t *testing.T) {
 	l := New()
-	e1 := l.Push(1)
-	e2 := l.Push(2)
+	e1 := l.PushBack(1)
+	e2 := l.PushBack(2)
 
 	checkListPointers(t, l, []*Element{e1, e2})
-	e := l.First()
+	e := l.Front()
 	l.Remove(e)
 
 	checkListPointers(t, l, []*Element{e2})
@@ -309,14 +313,14 @@ func TestRemove(t *testing.T) {
 
 func TestIssue4103(t *testing.T) {
 	l1 := New()
-	l1.Push(1)
-	l1.Push(2)
+	l1.PushBack(1)
+	l1.PushBack(2)
 
 	l2 := New()
-	l2.Push(3)
-	l2.Push(4)
+	l2.PushBack(3)
+	l2.PushBack(4)
 
-	e := l1.First()
+	e := l1.Front()
 	l2.Remove(e) // l2 should not change because e is not an element of l2
 	if n := l2.Len(); n != 2 {
 		t.Errorf("l2.Len() = %d, want 2", n)
@@ -330,10 +334,10 @@ func TestIssue4103(t *testing.T) {
 
 func TestIssue6349(t *testing.T) {
 	l := New()
-	l.Push(1)
-	l.Push(2)
+	l.PushBack(1)
+	l.PushBack(2)
 
-	e := l.First()
+	e := l.Front()
 	l.Remove(e)
 	if e.Value != 1 {
 		t.Errorf("e.value = %d, want 1", e.Value)
@@ -348,10 +352,10 @@ func TestIssue6349(t *testing.T) {
 
 func TestMove(t *testing.T) {
 	l := New()
-	e1 := l.Push(1)
-	e2 := l.Push(2)
-	e3 := l.Push(3)
-	e4 := l.Push(4)
+	e1 := l.PushBack(1)
+	e2 := l.PushBack(2)
+	e3 := l.PushBack(3)
+	e4 := l.PushBack(4)
 
 	l.MoveAfter(e3, e3)
 	checkListPointers(t, l, []*Element{e1, e2, e3, e4})
@@ -388,7 +392,7 @@ func TestZeroList(t *testing.T) {
 	checkList(t, l1, []interface{}{1})
 
 	var l2 = new(List)
-	l2.Push(1)
+	l2.PushBack(1)
 	checkList(t, l2, []interface{}{1})
 
 	var l3 = new(List)
@@ -403,9 +407,9 @@ func TestZeroList(t *testing.T) {
 // Test that a list l is not modified when calling InsertBefore with a mark that is not an element of l.
 func TestInsertBeforeUnknownMark(t *testing.T) {
 	var l List
-	l.Push(1)
-	l.Push(2)
-	l.Push(3)
+	l.PushBack(1)
+	l.PushBack(2)
+	l.PushBack(3)
 	l.InsertBefore(1, new(Element))
 	checkList(t, &l, []interface{}{1, 2, 3})
 }
@@ -413,9 +417,9 @@ func TestInsertBeforeUnknownMark(t *testing.T) {
 // Test that a list l is not modified when calling InsertAfter with a mark that is not an element of l.
 func TestInsertAfterUnknownMark(t *testing.T) {
 	var l List
-	l.Push(1)
-	l.Push(2)
-	l.Push(3)
+	l.PushBack(1)
+	l.PushBack(2)
+	l.PushBack(3)
 	l.InsertAfter(1, new(Element))
 	checkList(t, &l, []interface{}{1, 2, 3})
 }
@@ -423,10 +427,10 @@ func TestInsertAfterUnknownMark(t *testing.T) {
 // Test that a list l is not modified when calling MoveAfter or MoveBefore with a mark that is not an element of l.
 func TestMoveUnknownMark(t *testing.T) {
 	var l1 List
-	e1 := l1.Push(1)
+	e1 := l1.PushBack(1)
 
 	var l2 List
-	e2 := l2.Push(2)
+	e2 := l2.PushBack(2)
 
 	l1.MoveAfter(e1, e2)
 	checkList(t, &l1, []interface{}{1})
